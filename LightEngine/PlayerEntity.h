73bd8e6f3a31b61ee70c4988b27326021b7464d3
Gotvna <92024828+Gotvna@users.noreply.github.com>
2025-01-13 14:13:48 +0100

    add getspeed to Entity + add missing functions

--- LightEngine/PlayerEntity.h
+++ LightEngine/PlayerEntity.h
@@ -5,5 +5,19 @@
 class PlayerEntity : public Entity
 {
 public:
+    bool IsMarked() const { return mIsMarked; };
+    void SetIsMarked(bool isMarked) { mIsMarked = isMarked; };
 
+    bool HasBall() const { return mHasBall; };
+    void SetHasBall(bool hasBall) { mHasBall = hasBall; };
+
+    const std::vector<PlayerEntity*>& GetTeammates() const { return mTeammates; };
+    void SetTeammates(const std::vector<PlayerEntity*>& teammates) { mTeammates = teammates; };
+    const std::vector<PlayerEntity*>& GetOpponents() const { return mOpponents; };
+    void SetOpponents(const std::vector<PlayerEntity*>& opponents) { mOpponents = opponents; };
+private:
+    bool mIsMarked = false;
+    bool mHasBall = false;
+    std::vector<PlayerEntity*> mTeammates;
+    std::vector<PlayerEntity*> mOpponents;
 };
