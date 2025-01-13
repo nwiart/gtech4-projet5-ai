86eb4bf26b1b8f279a53a5bd1ead5d158672191f
nwiart <nwiart@gaming.tech>
2025-01-13 14:02:54 +0100

    Reorganize funcitons

--- LightEngine/SampleScene.cpp
+++ LightEngine/SampleScene.cpp
@@ -9,33 +9,8 @@
 
 void SampleScene::OnInitialize()
 {
-	const float playerRadius = 34.0F;
-	const float ballRadius = 20.0F;
-
-	const int wy = GetWindowHeight();
-	const int hy = GetWindowHeight() / 2;
-
-	// Player placement.
-	const float pos[5][2] = {
-		{ 80.0F,  80.0F },
-		{ 200.0F, hy - 240.0F },
-		{ 260.0F, hy },
-		{ 200.0F, hy + 240.0F },
-		{ 80.0F,  wy - 80.0F },
-	};
-
-	for (int i = 0; i < 5; i++) {
-		PlayerEntity* p0 = CreateEntity<PlayerEntity>(playerRadius, sf::Color::Green);
-		p0->SetPosition(pos[i][0], pos[i][1]);
-		p0->SetRigidBody(true);
-
-		PlayerEntity* p1 = CreateEntity<PlayerEntity>(playerRadius, sf::Color::Red);
-		p1->SetPosition(GetWindowWidth() - pos[i][0], pos[i][1]);
-		p1->SetRigidBody(true);
-	}
-
-	BallEntity* ball = CreateEntity<BallEntity>(ballRadius, sf::Color(255, 200, 0));
-	ball->SetPosition(400, 200);
+	CreatePlayersAndBall();
+	ResetPlayers();
 
 	pEntitySelected = nullptr;
 }
@@ -66,6 +41,45 @@ void SampleScene::TrySetSelectedEntity(DummyEntity* pEntity, int x, int y)
 		return;
 
 	pEntitySelected = pEntity;
+}
+
+void SampleScene::CreatePlayersAndBall()
+{
+	const float playerRadius = 34.0F;
+	const float ballRadius = 20.0F;
+
+	for (int i = 0; i < 5; i++) {
+		PlayerEntity* p0 = CreateEntity<PlayerEntity>(playerRadius, sf::Color::Green);
+		p0->SetRigidBody(true);
+		pPlayersTeam1[i] = p0;
+
+		PlayerEntity* p1 = CreateEntity<PlayerEntity>(playerRadius, sf::Color::Red);
+		p1->SetRigidBody(true);
+		pPlayersTeam2[i] = p1;
+	}
+
+	pBall = CreateEntity<BallEntity>(ballRadius, sf::Color(255, 200, 0));
+	pBall->SetPosition(400, 200);
+}
+
+void SampleScene::ResetPlayers()
+{
+	const int wy = GetWindowHeight();
+	const int hy = GetWindowHeight() / 2;
+
+	// Player placement.
+	const float pos[5][2] = {
+		{ 80.0F,  80.0F },
+		{ 200.0F, hy - 240.0F },
+		{ 260.0F, hy },
+		{ 200.0F, hy + 240.0F },
+		{ 80.0F,  wy - 80.0F },
+	};
+
+	for (int i = 0; i < 5; i++) {
+		pPlayersTeam1[i]->SetPosition(pos[i][0], pos[i][1]);
+		pPlayersTeam2[i]->SetPosition(GetWindowWidth() - pos[i][0], pos[i][1]);
+	}
 }
 
 void SampleScene::OnUpdate()
--- LightEngine/SampleScene.h
+++ LightEngine/SampleScene.h
@@ -11,8 +11,15 @@ class SampleScene : public Scene
 {
 	Entity* pEntitySelected;
 
+	PlayerEntity* pPlayersTeam1[5];
+	PlayerEntity* pPlayersTeam2[5];
+	BallEntity* pBall;
+
 private:
 	void TrySetSelectedEntity(DummyEntity* pEntity, int x, int y);
+
+	void CreatePlayersAndBall();
+	void ResetPlayers();
 
 public:
 	void OnInitialize() override;
