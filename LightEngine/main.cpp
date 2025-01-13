d5e9b13f16214b329f45ee5eb4c364269cfa8f4e
nwiart <nwiart@gaming.tech>
2025-01-13 14:13:04 +0100

    Changed back color

--- LightEngine/main.cpp
+++ LightEngine/main.cpp
@@ -12,7 +12,7 @@ int main()
 {
     GameManager* pInstance = GameManager::Get();
 
-	pInstance->CreateWindow(1280, 720, "SampleScene", 60, sf::Color::Black);
+	pInstance->CreateWindow(1280, 720, "SampleScene", 60, sf::Color(40, 70, 120));
 	
 	pInstance->LaunchScene<SampleScene>();
 
